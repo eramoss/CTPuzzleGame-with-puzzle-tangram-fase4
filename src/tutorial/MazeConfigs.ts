1d0fda0f735015a0849fb8dc0ad6f234d9341366
Cassiano Viana <cassiano.pvianna@gmail.com>
2020-11-07 22:42:43 -0300

    Adicionado tutorial de fase com giro

--- src/tutorial/MazeConfigs.ts
+++ src/tutorial/MazeConfigs.ts
@@ -17,6 +17,20 @@ export default class MazeConfigs {
     gridCellWidth: number;
     codeEditor: CodeEditor;
 
+    fnGetArrowUp = () => {
+        return joinChilds(this.codeEditor.toolboxRows, (t) => t.flow.children)
+            .find(c => c.texture.key == 'arrow-up')
+    };
+    fnGetArrowLeft = () => {
+        return joinChilds(this.codeEditor.toolboxRows, (t) => t.flow.children)
+            .find(c => c.texture.key == 'arrow-left')
+    };
+    fnGetArrowRight = () => {
+        return joinChilds(this.codeEditor.toolboxRows, (t) => t.flow.children)
+            .find(c => c.texture.key == 'arrow-right')
+    };
+    fnGetBtnPlay = () => this.codeEditor.btnPlay.sprite;
+
     constructor(scene: Scene,
         grid: AlignGrid,
         codeEditor: CodeEditor,
@@ -36,8 +50,9 @@ export default class MazeConfigs {
 
         this.phases = new Array<MazePhase>();
 
+        this.phases.push(this.createPhaseEasyArrowUpAndRight());
         this.phases.push(this.createPhaseEasyArrowUp());
-        this.phases.push(this.createPhaseEasyArrowUpAndLeft());
+        this.phases.push(this.createPhaseEasyArrowUpTwoTimes());
     }
 
     getNextPhase(): MazePhase {
@@ -90,20 +105,14 @@ export default class MazeConfigs {
                 this.gridCenterX, this.gridCenterY, this.gridCellWidth
             );
 
-            const fnGetArrowUp = () => {
-                return joinChilds(this.codeEditor.toolboxRows, (t) => t.flow.children)
-                    .find(c => c.texture.key == 'arrow-up')
-            };
-            const fnGetBtnPlay = () => this.codeEditor.btnPlay.sprite;
-
-            phase.addClickTutorialAction(fnGetArrowUp);
-            phase.addClickTutorialAction(fnGetBtnPlay);
+            phase.addClickTutorialAction(this.fnGetArrowUp);
+            phase.addClickTutorialAction(this.fnGetBtnPlay);
         }
 
         return phase;
     }
 
-    private createPhaseEasyArrowUpAndLeft() {
+    private createPhaseEasyArrowUpTwoTimes() {
         const phase = new MazePhase(this.scene, this.grid);
         phase.dudeFacedTo = 'right'
         phase.dudeStartPosition = { col: 1, row: 3 }
@@ -154,7 +163,56 @@ export default class MazeConfigs {
             phase.addClickTutorialAction(fnGetArrowUp);
             phase.addClickTutorialAction(fnGetBtnPlay);
         }
-        /* const btnStep = this.codeEditor.btnStep.sprite; */
+        return phase;
+    }
+
+    private createPhaseEasyArrowUpAndRight() {
+        const phase = new MazePhase(this.scene, this.grid);
+        phase.dudeFacedTo = 'right'
+        phase.dudeStartPosition = { col: 1, row: 2 }
+
+        let baseMatrix = [
+            ['tile', 'tile', 'tile', 'tile', 'tile', 'tile', 'tile'],
+            ['tile', 'tile', 'tile', 'tile', 'tile', 'tile', 'tile'],
+            ['tile', 'tile', 'tile', 'tile', 'tile', 'tile', 'tile'],
+            ['tile', 'tile', 'tile', 'tile', 'tile', 'tile', 'tile'],
+            ['tile', 'tile', 'tile', 'tile', 'tile', 'tile', 'tile'],
+            ['tile', 'tile', 'tile', 'tile', 'tile', 'tile', 'tile'],
+            ['tile', 'tile', 'tile', 'tile', 'tile', 'tile', 'tile'],
+        ];
+
+        let obstaclesMatrix = [
+            ['null', 'null', 'null', 'null', 'null', 'null', 'null'],
+            ['null', 'null', 'null', 'null', 'null', 'null', 'null'],
+            ['null', 'null', 'null', 'null', 'null', 'null', 'null'],
+            ['null', 'null', 'null', 'coin', 'null', 'null', 'null'],
+            ['null', 'null', 'null', 'null', 'null', 'null', 'null'],
+            ['null', 'null', 'null', 'null', 'null', 'null', 'null'],
+            ['null', 'null', 'null', 'null', 'null', 'null', 'null'],
+        ];
+
+        phase.setupTutorialsAndObjectsPositions = () => {
+
+            phase.obstacles = new Matrix(
+                this.scene,
+                this.matrixMode,
+                obstaclesMatrix,
+                this.gridCenterX, this.gridCenterY, this.gridCellWidth
+            );
+
+            phase.ground = new Matrix(
+                this.scene,
+                this.matrixMode,
+                baseMatrix,
+                this.gridCenterX, this.gridCenterY, this.gridCellWidth
+            );
+
+            phase.addClickTutorialAction(this.fnGetArrowUp);
+            phase.addClickTutorialAction(this.fnGetArrowUp);
+            phase.addClickTutorialAction(this.fnGetArrowRight);
+            phase.addClickTutorialAction(this.fnGetArrowUp);
+            phase.addClickTutorialAction(this.fnGetBtnPlay);
+        }
         return phase;
     }
 }

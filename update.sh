956867edc07c574838ec9d9a26ab5f76deb5406d
Cassiano Viana <cassiano.pviana@gmail.com>
2022-10-02 22:05:23 -0300

    Update dockerfile

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@ docker container stop ctpuzzlegame
 docker container rm ctpuzzlegame
 docker image rm ctpuzzlegame:1.0
 docker image build -t ctpuzzlegame:1.0 .
-docker run --name ctpuzzlegame -p 3339:8080 ctpuzzlegame:1.0 npx node-static -a 0.0.0.0 dist
+docker run --name ctpuzzlegame -p 3339:8080 ctpuzzlegame:1.0 npx node-static dist -a 0.0.0.0

983cbcbf087aff0abe8230e5e64f53ab56be25fb
Cassiano Viana <cassiano.pviana@gmail.com>
2022-10-02 21:00:25 -0300

    update

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 git pull
 docker container stop ctplat
 docker container rm ctplat
-docker image rm ctplatform:1.0
-docker image build -t ctplatform:1.0 .
-docker run --name ctplat -p 3339:8080 ctplatform:1.0 http-server dist
+docker image rm ctpuzzlegame:1.0
+docker image build -t ctpuzzlegame:1.0 .
+docker run --name ctpuzzlegame -p 3339:8080 ctpuzzlegame:1.0 http-server dist
